362ac5b7c08afe679274f6af73e3581ec5298fe5
奇点 <3099234061@qq.com>
2022-09-25 21:20:39 +0800

    updata

--- qidian/js/js1.js
+++ qidian/js/js1.js
@@ -1,7 +1,26 @@
 function ban(){
     alert("暂未开放")
 }
+let name = ""
 function destory(){
     document.getElementById("bt").style.display = "none";
     document.getElementById("tapp").style.filter = "none";
+}
+function confirm() {
+    document.getElementById("d_log").style.display = "none";
+    alert("您已成功预定'   "+this.name+"   '请尽快联系作者付款");
+}
+function buy(a,b){
+    pic(b)
+    this.name = a;
+    console.log(this.name)
+    document.getElementById("d_log").style.display = "block";
+}
+let s = "Image/a1.png";
+function pic    (e) {
+    document.getElementById("d_img").src = e;
+    console.log(s)
+}
+function cancel(){
+    document.getElementById("d_log").style.display = "none"
 }
